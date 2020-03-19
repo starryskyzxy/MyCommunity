d239f91265b33f61f95ed73798a2d7b2947e521d
starryskyzxy <1138141748@qq.com>
2020-03-19 08:14:56 +0800

    extract github config from code to properties

---
+++ src/main/java/com/zxy/config/MyMvcConfig.java
@@ -0,0 +1,17 @@
+package com.zxy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//扩展springmvc
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+    }
+}
---
+++ src/main/java/com/zxy/dto/GithubUser.java
@@ -0,0 +1,43 @@
+package com.zxy.dto;
+
+public class GithubUser {
+
+    private String name;
+    private Long id;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "GithubUser{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", bio='" + bio + '\'' +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
+
+
